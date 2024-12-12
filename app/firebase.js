80f7688034c1298f0ee00c6a39782c8c9c6dd333
SV-1337 <shubh.varshney08@gmail.com>
2024-12-11 19:31:21 -0600

    Update firebase.js to add new database

--- app/firebase.js
+++ app/firebase.js
@@ -1,18 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC4_RLOFNrxoCppSfh5EIdMPnXjGd5IT_c",
-    authDomain: "headstarter2.firebaseapp.com",
-    projectId: "headstarter2",
-    storageBucket: "headstarter2.appspot.com",
-    messagingSenderId: "780947559506",
-    appId: "1:780947559506:web:b0d274b2743ff49a1cfb8b"
+  apiKey: "AIzaSyA0LbXilrueCEpnz2iw7yOTOLd-XtfDVnQ",
+  authDomain: "pantry-7212f.firebaseapp.com",
+  projectId: "pantry-7212f",
+  storageBucket: "pantry-7212f.firebasestorage.app",
+  messagingSenderId: "519449019260",
+  appId: "1:519449019260:web:898ec97b6b2ecd1b7a9af9"
 };
 
 // Initialize Firebase
